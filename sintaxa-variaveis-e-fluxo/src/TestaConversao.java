d34bbc27a60bf50bd0cdac2939e368e16c78cd3a
FelipeLuzia <felipe.luzia@hotmail.com>
2023-10-19 10:35:55 -0300

    04 07 Conversões e outros tipos

---
+++ sintaxa-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,21 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+		
+		float pontoFlutuante = 3.14f;
+		
+		int x = 2000000000;
+		long numeroGrande = 3216584614364654890l;
+		short valorPequeno = 25611;
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+
+}
